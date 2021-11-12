97d7b6e0cbfe2af949f29c72e0439ed38dd83014
mikkels <mikkelsjolie@gmail.com>
2021-11-12 11:39:07 +0100

    WIP

---
+++ src/ElectronicFarts/GamerLoop.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using SadConsole;
+using Console = SadConsole.Console;
+
+namespace ElectronicFarts
+{
+    public class GameLoop
+    {
+        public const int Width = 30;
+        public const int Height = 30;
+        private static Player player;
+
+        static void Main(string[] args)
+        {
+            SadConsole.Game.Create(Width, Height);
+            SadConsole.Game.OnInitialize = Init;
+            SadConsole.Game.OnUpdate = Update;
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        private static void Update(GameTime obj)
+        {
+            if (Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.F5))
+            {
+                Settings.ToggleFullScreen();
+            }
+
+            //if (Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Up))
+            //{
+            //    player.MoveBy(new Point(0, -1));
+            //}
+
+            //if (Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Down))
+            //{
+            //    player.MoveBy(new Point(0, 1));
+            //}
+
+            if (Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Left))
+            {
+                player.MoveBy(new Point(-1, 0));
+            }
+
+            if (Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Right))
+            {
+                player.MoveBy(new Point(1, 0));
+            }
+        }
+
+        private static void Init()
+        {
+            Console startingConsole = new ScrollingConsole(Width, Height, Global.FontDefault, new Rectangle(0, 0, Width, Height));
+            
+            SadConsole.Global.CurrentScreen = startingConsole;
+            CreatePlayer();
+            startingConsole.Children.Add(player);
+        }
+
+        private static void CreatePlayer()
+        {
+            player = new Player(Color.Yellow, Color.Transparent, 1, 4, 4);
+            player.Position = new Point(14, 29);
+        }
+    }
+}
---
+++ src/ElectronicFarts/Player.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ElectronicFarts
+{
+    public class Player : Actor
+    {
+        public Player(Color foreground, Color background, int glyph, int width =4, int height = 4) : base(foreground, background, glyph, width, height)
+        {
+        }
+    }
+}
