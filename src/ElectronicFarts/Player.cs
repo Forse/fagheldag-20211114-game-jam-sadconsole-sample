dd892be154d0c1f7e36fa09e907202d89f03cab5
Svein Erik Voll <svein.erik.voll@forse.no>
2021-11-12 16:42:59 +0100

    shot

--- src/ElectronicFarts/Player.cs
+++ src/ElectronicFarts/Player.cs
@@ -116,4 +116,11 @@ namespace ElectronicFarts
             Name = "Wall";
         }
     }
+    
+    public class Shot : Actor
+    {
+        public Shot(Color foreground, Color background, int glyph, int width = 4, int height = 4) : base(foreground, background, glyph, width, height)
+        {
+        }
+    }
 }
