98907dddabbc08771afda2c9ee3257d822957f3b
Svein Erik Voll <svein.erik.voll@forse.no>
2021-11-13 11:40:49 +0100

    split player file into several files

---
+++ src/ElectronicFarts/HealthEntity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ElectronicFarts
+{
+    public class HealthEntity : Actor
+    {
+        public HealthEntity(Color foreground, Color background, int glyph, int width = 1, int height = 1) : base(foreground, background, glyph, width, height)
+        {
+        }
+    }
+}
---
+++ src/ElectronicFarts/Shot.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ElectronicFarts
+{
+    public class Shot : Actor
+    {
+        public Shot(Color foreground, Color background, int glyph, int width = 4, int height = 4) : base(foreground, background, glyph, width, height)
+        {
+        }
+    }
+}
